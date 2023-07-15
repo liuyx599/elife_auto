7437bd2e336080b952ed39dc95edba04f8bc08df
liuyx599 <995066544@qq.com>
2023-07-15 11:45:12 +0800

    Initial commit

---
+++ cases/test_管理界面登录.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+# author: onebear
+# datetime: 2023/7/15 11:43
+# description:
---
+++ cfg.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# author: onebear
+# datetime: 2023/7/15 11:40
+# description:  配置文件
+
+
+SMP_ADDRESS = 'http://127.0.0.1:8234/'
+
+SMP_URL_LOGIN = f'{SMP_ADDRESS}/login.html'
